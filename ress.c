7201be17883622ebd0a6510dba09b6359898d171
jaskieerr <nizar.zairat@gmail.com>
2023-10-19 13:38:49 -0700

    nop

---
+++ ress.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does jackshit
+ * @stack: top node double pointer
+ * @lnn: line number
+ */
+void nop(stack_t **stack, unsigned int lnn)
+{
+	(void)stack;
+	(void)lnn;
+}
